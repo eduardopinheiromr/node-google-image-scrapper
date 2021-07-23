e95477d4decf723881cfe3ca2475f3378e627a4f
Eduardo Pinheiro <3dup1n@gmail.com>
2021-07-23 01:37:43 -0300

    feat: enable cors

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const getImagesHandler = require("./src/controllers/scrapper");
 const PORT = process.env.PORT || 5000;
 
+// INIT
 const app = express();
 
+// MIDDLEWARES
+app.use(cors());
 app.use(express.static("public"));
 
+// ROUTES
 app.get("/", (req, res) => res.send("Home"));
-
 app.get("/image/:query/:quantity?", getImagesHandler);
 
+// START
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
